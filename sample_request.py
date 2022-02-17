546c568ef5f92265b105f6f1f447dd08336bba4c
Raj Pathak <73082957+rppth@users.noreply.github.com>
2022-02-17 09:41:47 -0800

    Update sample_request.py

--- sample_request.py
+++ sample_request.py
@@ -22,7 +22,7 @@ def s3_pdf_textract(s3_uri):
 if __name__ == "__main__":
     local_or_s3 = input("Type 'local' to input a local PDF file. Otherwise, type 's3' to input an S3 URI for your PDF file: ")
     if local_or_s3 == "local":
-        input_file_path = "samplep.pdf" #replace this with the local path of your document
+        input_file_path = "samplepages.pdf" #replace this with the local path of your document
         local_pdf_textract(input_file_path)
     elif local_or_s3 == "s3":
         s3_uri = "s3://{bucket_name}/sample.pdf" #replace this with the s3 uri of document
